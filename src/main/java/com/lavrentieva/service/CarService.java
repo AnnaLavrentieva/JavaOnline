7e56337cf653e190e3420210585b5486e89b976b
Ann Lav <foed111@gmail.com>
2022-11-16 10:15:27 +0200

    hw6

--- src/main/java/com/lavrentieva/service/CarService.java
+++ src/main/java/com/lavrentieva/service/CarService.java
@@ -5,13 +5,15 @@ import com.lavrentieva.model.Car;
 import java.util.Random;
 
 public class CarService {
-    Car car;
+
     public Car create() {
+        Car car;
         car = new Car("man " + new Random().nextInt(1000),
                 "eng " + new Random().nextInt(1000),
                 "col " + new Random().nextInt(1000));
         return car;
     }
+
     public void print(Car car) {
         System.out.println("Manufacturer: " + car.getManufacturer() + ", Engine: " + car.getEngine() +
                 ", Color: " + car.getColor() + ", Count: " + car.getCount() + ", Price: "
